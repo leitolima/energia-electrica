5032b909dbf9f35a9c94bb20562574bb118d60dd
Leonel Lima <leo-lima-08@hotmail.com>
2020-10-21 18:36:51 -0300

    comienzo formulario carga de datos

--- src/pages/informes/Carga.js
+++ src/pages/informes/Carga.js
@@ -1,9 +1,122 @@
-import React from 'react'
+import React from 'react';
 
 const Carga = () => {
     return (
-        <div>
-            
+        <div className="container-fluid mt-2">
+            <div className="row">
+                <h1 className="d-block">Carga de datos</h1>
+            </div>
+            <div className="row mt-2">
+                <div className="form-check form-check-inline">
+                    <input className="form-check-input" defaultChecked="true" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
+                    <label className="form-check-label" htmlFor="inlineRadio1">Energía Producida</label>
+                </div>
+                <div className="form-check form-check-inline ml-3">
+                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+                    <label className="form-check-label" htmlFor="inlineRadio2">Energía Consumida</label>
+                </div>
+            </div>
+                {/* ENERGIA PRODUCIDA */}
+            <div id="producida" className="row jumbotron mt-2">
+                <div className="col-md-6 col-lg-6 col-xl-6">
+                    <label htmlFor="pro_central">Central Productora: </label>
+                    <select 
+                        className="form-control" 
+                        id="pro_central"
+                    >
+                        <option value="0">Seleccione central</option>
+                    </select>
+                </div>
+                <div className="col-md-6 col-lg-6 col-xl-6">
+                    <div className="form-group">
+                        <label htmlFor="pro_vol_energia">Volumen de energía: </label>
+                        <input 
+                            type="text"
+                            className="form-control" 
+                            id="pro_vol_energia"
+                        />
+                    </div>
+                </div>
+                <div className="col-md-6 col-lg-6 col-xl-6">
+                    <div className="form-group">
+                        <label htmlFor="pro_fecha_carga">Fecha de carga: </label>
+                        <input 
+                            type="date" 
+                            className="form-control" 
+                            id="pro_fecha"
+                            disabled
+                        />
+                    </div>
+                </div>
+                <div className="col-md-6 col-lg-6 col-xl-6">
+                    <div className="form-group">
+                        <label htmlFor="pro_fecha_consumo">Fecha de consumo: </label>
+                        <input 
+                            type="date" 
+                            className="form-control" 
+                            id="pro_fecha_consumo"
+                        />
+                    </div>
+                </div>
+                <div className="col-md-6 col-lg-6 col-xl-6">
+                    <label htmlFor="pro_provincia">Provincia: </label>
+                    <select 
+                        className="form-control" 
+                        id="pro_provincia"
+                    >
+                        <option value="0">Seleccione la provincia</option>
+                    </select>
+                </div>
+                <div className="col-md-6 col-lg-6 col-xl-6">
+                    <label htmlFor="pro_zona">Zona: </label>
+                    <select 
+                        className="form-control" 
+                        id="pro_zona"
+                    >
+                        <option value="0">Seleccione la zona</option>
+                    </select>
+                </div>
+                <div className="col-md-12 col-lg-12 col-xl-12 my-3">
+                    <h3>Consumidores</h3>
+                </div>
+                <div className="col-md-4 col-lg-4 col-xl-4">
+                    <div className="form-group">
+                        <label htmlFor="pro_particulares">Particulares: </label>
+                        <input 
+                            type="number"
+                            className="form-control" 
+                            id="pro_particulares"
+                            disabled
+                        />
+                    </div>
+                </div>
+                <div className="col-md-4 col-lg-4 col-xl-4">
+                    <div className="form-group">
+                        <label htmlFor="pro_empresas">Empresas: </label>
+                        <input 
+                            type="number"
+                            className="form-control" 
+                            id="pro_empresas"
+                            disabled
+                        />
+                    </div>
+                </div>
+                <div className="col-md-4 col-lg-4 col-xl-4">
+                    <div className="form-group">
+                        <label htmlFor="pro_instituciones">Instituciones: </label>
+                        <input 
+                            type="number"
+                            className="form-control" 
+                            id="pro_instituciones"
+                            disabled
+                        />
+                    </div>
+                </div>
+            </div>
+            {/* ENERGIA CONSUMIDA */}
+            <div id="consumida" className="row">
+
+            </div>
         </div>
     )
 }
